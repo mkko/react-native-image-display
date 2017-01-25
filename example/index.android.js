a36c444cbcc356c3e8d6397ac4dd69284cd17f1d
Mikko Välimäki <mkko1373@gmail.com>
2017-01-25 14:20:24 +0100

    Combine examples

--- example/index.android.js
+++ example/index.android.js
@@ -4,73 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ScrollView,
-  Modal
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import App from './app';
 
-import Lightbox from './Lightbox';
-
-const images = [{
-  uri: 'https://upload.wikimedia.org/wikipedia/commons/2/29/Wurstplatte.jpg'
-  // uri: 'file:///Users/mval/Desktop/Wurstplatte.jpg'
-}];
-
-export default class example extends Component {
-
-  componentWillMount() {
-    Image.prefetch(images[0].uri);
-  }
-
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        <Text style={styles.welcome}>
-          Click the photo
-        </Text>
-        <Lightbox images={images}>
-          <Image
-            style={styles.contain}
-            resizeMode="cover"
-            source={{ uri: images[0].uri }}
-          />
-        </Lightbox>
-      </ScrollView>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    paddingTop: 48,
-    backgroundColor: '#F5FCFF',
-  },
-  contain: {
-    flex: 1,
-    height: 150,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginVertical: 10,
-  },
-  bannerImageContainer: {
-    backgroundColor: 'red',
-    height: 200,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('example', () => example);
+AppRegistry.registerComponent('example', () => App);
